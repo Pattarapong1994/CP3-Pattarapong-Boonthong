b0597dbd9a8521659db662ddfadb0866dc23d073
Pattarapong <porz.porz.porz@gmail.com>
2020-02-26 15:16:20 +0700

    Add score
    FE = Foundation English
    GB = General Business
    ICS = Introduction to Computer Systems
    CP = Computer Programming

---
+++ Exercise4_Pattarapong_B.py
@@ -0,0 +1,13 @@
+score_FE = 78
+"""FE = Foundation English"""
+score_GB = 64
+"""GB = General Business"""
+score_ICS = 74
+"""ICS = Introduction to Computer Systems"""
+score_CP  = 88
+"""CP = Computer Programming"""
+print("--- Your Score ---")
+print("Foundation Engish :" , score_FE)
+print("General Business :" , score_GB)
+print("Introduction to Computer Systems :" , score_ICS)
+print("Computer Programming :" , score_CP)
