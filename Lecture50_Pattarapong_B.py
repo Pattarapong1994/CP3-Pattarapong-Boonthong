1c01a5946f690f44215061d4e61584f2ba989865
Pattarapong <porz.porz.porz@gmail.com>
2020-02-27 12:53:51 +0700

    add function

---
+++ Lecture50_Pattarapong_B.py
@@ -0,0 +1,12 @@
+def addNumber(x,y):
+    print(x + y)
+def minusNumber(x,y):
+    print(x-y)
+def multipleNumber(x,y):
+    print(x*y)
+def divideNumber(x,y):
+    print(x/y)
+addNumber(100,10)
+minusNumber(100,15)
+multipleNumber(5,10)
+divideNumber(50,5)
