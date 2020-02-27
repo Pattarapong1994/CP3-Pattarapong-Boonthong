eede45c9829db1c06166f935595b493a48f82fbf
Pattarapong <porz.porz.porz@gmail.com>
2020-02-27 17:36:01 +0700

    edit code

--- Exercise8_Pattarapong_B.py
+++ Exercise8_Pattarapong_B.py
@@ -2,31 +2,23 @@ user_login = input("User ID: ")
 password_login = input("PASSWORD: ")
 if user_login == "admin" and password_login == "123456":
     print("----Welcom to Porz Shop----")
-    a = "Banana"
-    a_price = 10
-    b = "Apple"
-    b_price = 15
-    c = "Orange"
-    c_price = 7
-    d = "Mango"
-    d_price = 16
-    print("1.",a,"Price:",a_price,"THB")
-    print("2.",b,"Price:",b_price,"THB")
-    print("3.",c,"Price:",c_price,"THB")
-    print("4.",d,"Price:",d_price,"THB")
+    Banana_price = 10
+    Apple_price = 15
+    Orange_price = 7
+    Mango_price = 16
+    print("1.Banana Price:", Banana_price, "THB")
+    print("2.Apple Price:", Apple_price, "THB")
+    print("3.Orange Price:", Orange_price, "THB")
+    print("4.Mango Price:", Mango_price, "THB")
     user_Select = int(input("Select Number: "))
     amount = int(input("Amount:"))
     if user_Select == 1:
-        print(a,"Amount:", amount,"ea")
-        print("Total Price:",a_price * amount,"THB")
+        print("Banana Amount:", amount, "ea", "Total Price:", Banana_price * amount, "THB")
     elif user_Select == 2:
-        print(b,"Amount:", amount,"ea")
-        print("Total Price:",b_price * amount,"THB")
+        print("Apple Amount:", amount,"ea", "Total Price:", Apple_price * amount, "THB")
     elif user_Select == 3:
-        print(c,"Amount:", amount,"ea")
-        print("Total Price:", c_price * amount,"THB")
+        print("Orange Amount:", amount,"ea", "Total Price:", Orange_price * amount, "THB")
     elif user_Select == 4:
-        print(d,"Amount:", amount,"ea")
-        print("Total Price:",d_price * amount,"THB")
+        print("Mongo Amount:", amount,"ea", "Total Price:", Mango_price * amount, "THB")
 else:
     print("USER or PASSWORD is wrong!!")
